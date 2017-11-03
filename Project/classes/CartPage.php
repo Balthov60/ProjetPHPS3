a3b80d7262f7a6bf58a0f36bbd8063fa7050c849
Balthov60 <Balthazarfrolin@gmail.com>
2017-11-04 00:30:20 +0100

    Clean Code

--- Project/classes/CartPage.php
+++ Project/classes/CartPage.php
@@ -1,113 +1,71 @@
 <?php
 include_once("HeaderBar.php");
-include_once("AdminPanel.php");
 include_once("FooterBar.php");
+include_once("ImageHandler.php");
+
 class CartPage
 {
+    private $sqlService;
+
     function __construct(SQLServices $sqlService)
     {
+        $this->sqlService = $sqlService;
+
         new HeaderBar(true, false, "Cart", $sqlService);
 
-        $this->displayCart();
+        $this->displayCartContent();
 
         new FooterBar();
     }
 
-
-
-    /*** Display method ***/
-
-    function displayCart()
+    private function displayCartContent()
     {
-        $username = $_SESSION['user']['username'];
-        $cart = $this->getCartOf($username);
+        $cart = $this->getCartOf($_SESSION['user']['username']);
 
         if(empty($cart))
-            echo "<div id='cart-container'><p>You don't selected photos, you cart is empty!</p></div>";
-        else
-            $this->display($cart);
-    }
-
-    /**
-     * @param $userID
-     * @return data
-     */
-    function getCartOf($username)
-    {
-        $result = $this->sqlService->getData('cart', 'image_name', array("where" => "username = '$username'"));
-        return $result;
-    }
-
-
-    function display($cartArray)
-    {
-        echo "<div id='cart-container'>";
-
-        foreach($cartArray as $cartElement)
         {
-
-            echo "<div class='cart-element'>";
-
-            $this->displayImage($cartElement);
-
-            echo "<div class='details-container'>";
-
-            $this->displayDescription($cartElement);
-            $this->displayPrice($cartElement);
-
-            echo "</div>";
-
-            $this->displayRemoveButton($cartElement);
-
-            echo "</div>";
-
-            echo "<div class='divider-horizontal'></div>";
+            echo "<div id='cart-container'><p>Your cart is empty!</p></div>";
         }
-
-        echo "</div>";
-        $this->displayTotalOf($cartArray);
+        else
+        {
+            echo "<div id='cart-container'>";
+            foreach($cart as $cartItem)
+            {
+                $this->displayCartItem($cartItem);
+            }
+            echo "</div>";
+            $this->displayTotalOf($cart);
+        }
     }
 
+    /* Cart Item Methods */
 
-    function displayImage($cartElement)
-    {
-        $imageName = $cartElement['image_name'];
-        ImageHandler::displayCopyrightedImage($imageName);
+    private function displayCartItem($cartItem)
+    { ?>
+        <div class='cart-element'>
+            <?php ImageHandler::displayCopyrightedImage($cartItem['image_name']); ?>
+            <div class='details-container'>
+                <p><?php $this->getDescriptionOf($cartItem); ?></p>
+                <p><?php $this->getPriceOf($cartItem); ?></p>
+            </div>
+            <?php $this->displayRemoveButton($cartItem) ?>
+        </div>
+        <div class='divider-horizontal'></div>
+    <?php
     }
-
-    function displayDescription($cartElement)
+    function displayRemoveButton($cartItem)
     {
-        $description = $this->getDescriptionOf($cartElement);
-        echo "<p>$description</p>";
-    }
-
-    function displayPrice($cartElement)
-    {
-        $price = $this->getPriceOf($cartElement);
-        echo "<p>$price</p>";
-    }
-
-    function displayRemoveButton($cartElement)
-    {
-        $imageName = $cartElement['image_name'];
-        echo "<span class='remove-cart-span' id='remove-$imageName'>&times;</span>";
-    }
-
-    function displayTotalOf($cart)
-    {
-        $price = $this->getTotalPriceOf($cart);
-        $nbPicture = $this->getPictureNumberIn($cart);
-
-        echo "<p id='nb-picture-cart'>Number of picture selected :  $nbPicture</p></br><p>Total : $price € </p>";
+        echo "<span class='remove-cart-span' id='remove-{$cartItem['image_name']}'>&times;</span>";
     }
 
     function getPriceOf($cartElement)
     {
         $imageName = $cartElement['image_name'];
-        $result = $this->sqlService->getData('image', 'price', array("where" => "name_image = '$imageName'"));
+        $result = $this->sqlService->getData('image', 'price',
+            array("where" => "name_image = '$imageName'")
+        );
         return $result[0]['price'];
     }
-
     function getDescriptionOf($cartElement)
     {
         $imageName = $cartElement['image_name'];
@@ -115,22 +73,37 @@ class CartPage
         return $result[0]['description'];
     }
 
-    function getTotalPriceOf($cart)
-    {
-        $priceCounter = 0;
-        foreach ($cart as $cartElement)
-            $priceCounter += $this->getPriceOf($cartElement);
+    /* Global Cart Methods */
 
-        return $priceCounter;
+    function displayTotalOf($cart)
+    {
+        $price = $this->getTotalPriceOf($cart);
+        $pictureQty = $this->getPictureNumberIn($cart);
+
+        echo "<p id='nb-picture-cart'>Number of picture selected : $pictureQty</p>
+              </br><p>Total : $price €</p>";
     }
 
+    function getTotalPriceOf($cart)
+    {
+        $totalPrice = 0;
+        foreach ($cart as $cartElement)
+            $totalPrice += $this->getPriceOf($cartElement);
+
+        return $totalPrice;
+    }
     function getPictureNumberIn($cart)
     {
-        $pictureCounter = 0;
-        foreach ($cart as $cartElement)
-            $pictureCounter ++;
+        return sizeof($cart);
+    }
 
-        return $pictureCounter;
+    /**
+     * @param $username
+     * @return array of images
+     */
+    private function getCartOf($username)
+    {
+        return $this->sqlService->getData('cart', 'image_name', array("where" => "username = '$username'"));
     }
 }
 
