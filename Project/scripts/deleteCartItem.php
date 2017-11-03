65827ba774108aee5866268609fa80110924a7d0
LucasSecret <lucas.secret@orange.fr>
2017-11-03 19:44:48 +0100

    Implement DeleteCartItem.php script

--- Project/scripts/deleteCartItem.php
+++ Project/scripts/deleteCartItem.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sntri
- * Date: 03/11/2017
- * Time: 19:41
- */
+
+include_once("../classes/SQLServices.php");
+include_once("../classes/CartPanel.php");
+
+include("../includes/variables.inc.php");
+$sqlService = new SQLServices($host, $dbName, $user, $password);
+
+$imageName = getImageNameFrom($_GET['buttonID']);
+
+
+$sqlService->removeData('cart', "image_name = '$imageName'");
+
+header("Location: ../index.php?page=cart");
+
+function getImageNameFrom($buttonID)
+{
+    $removeID = "remove-";
+    $removeIDlength = strlen($removeID);
+    $imageName = substr($buttonID, $removeIDlength);
+    return $imageName;
+}
+
+?>
