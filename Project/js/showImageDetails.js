b859f35331a3b64721344ed77527b47660e88414
Balthov60 <Balthazarfrolin@gmail.com>
2017-11-04 02:16:12 +0100

    clean code

--- Project/js/showImageDetails.js
+++ Project/js/showImageDetails.js
@@ -1,114 +1,73 @@
 $(document).ready(function () {
     var imageID;
+
     $("img").click(function()
     {
         imageID = $(this).attr("id");
-        if(isDBPicture(imageID))
+        if(!isWebSiteGraphicPicture(imageID))
             insertModalContent($(this).attr("id"));
     });
 
     $(document).click(function(event)
     {
-       if(event.target.className == $(".modal").attr("class"))
+       if(event.target.className === $(".modal").attr("class"))
        {
            hideModal();
        }
-
-       else if(event.target.id == $("#add-cart-submit").attr("id"))
+       else if(event.target.id === $("#add-cart-submit").attr("id"))
        {
-           window.location.href = "scripts/addImageToCart.php?imageID=" + imageID;
+           var xmlhttp = new XMLHttpRequest();
+           xmlhttp.open("GET", "scripts/addImageToCart.php?imageID=" + imageID, true);
+           xmlhttp.send(null);
+           alert("added to cart");
        }
     });
-
-
 });
 
 
-function isDBPicture(imageID)
+/**
+ * Check If ImageID is part of real image or if it is just a website graphical component.
+ *
+ * @param imageID
+ * @returns {boolean}
+ */
+function isWebSiteGraphicPicture(imageID)
 {
-    if(imageID != "advanced-menu-button" && imageID != "logo" && imageID != "default-photo-user")
-        return true;
-
-    return false;
+    return imageID === "advanced-menu-button" || imageID === "logo";
 }
 
-/* Modal Content Insertion */
+/* Modal Displaying functions */
 
-/**
- *
- * @param imageSelectedID
- */
-function insertModalContent(imageSelectedID)
+function insertModalContent(imageID)
 {
-
-    insertSelectedImage(imageSelectedID);
-    insertSelectedImageDetails(imageSelectedID);
+    writeHTMLImageCode(extractImageName(imageID));
+    writeHTMLImageDetailsCode(extractImageName(imageID));
 }
 
-/**
- *
- * @param imageSelectedID
- */
-function insertSelectedImage(imageSelectedID)
-{
-    var imageName = getSelectedImageName(imageSelectedID);
-    writeHTMLImageCode(imageName);
-
-}
-
-/**
- *
- * @param imageSelectedID
- * @returns {Array.<T>|string|Blob|ArrayBuffer|*}
- */
-function getSelectedImageName(imageSelectedID)
-{
-    var imageIDPos = imageSelectedID.search("._image");
-    var realImageName = imageSelectedID.slice(0, imageIDPos);
-    return realImageName;
-}
-
-/**
- *
- * @param imageName
- */
 function writeHTMLImageCode(imageName)
 {
     var htmlCodeImage = "<img src=\"library/images_copyright/" + imageName + "\">";
     $(".modal #image-container").html(htmlCodeImage);
 
 }
-
-/**
- *
- * @param imageSelectedID
- */
-function insertSelectedImageDetails(imageSelectedID)
-{
-    var imageName = getSelectedImageName(imageSelectedID);
-    writeHTMLImageDetailsCode(imageName);
-}
-
-/**
- *
- * @param imageName
- */
 function writeHTMLImageDetailsCode(imageName)
 {
     getImageDetailsWithAJAX(imageName, displayData);
 }
 
+function extractImageName(imageID)
+{
+    var imageIDPos = imageID.search("._image");
+    return imageID.slice(0, imageIDPos);
+}
 
-/**
- *
- * @param imageName
- * @param callback
- */
+/* PHP Linked */
+
 function getImageDetailsWithAJAX(imageName, callback) //AJAX Method
 {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && (xmlhttp.status == 200 || xmlhttp.status == 0)) {
+        if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
             var responseText = xmlhttp.responseText;
             callback(responseText);
         }
@@ -116,32 +75,29 @@ function getImageDetailsWithAJAX(imageName, callback) //AJAX Method
     xmlhttp.open("GET", "scripts/getImageDetails.php?imageName=" + imageName, true);
     xmlhttp.send(null);
 }
-
-/**
- *
- * @param detailsString
- */
 function displayData(detailsString)
 {
     var detailsArray = extractDetailsFromString(detailsString);
     var description = detailsArray[0];
     var price = detailsArray[1];
 
-    var codeHtmlDetails = "<div id='desc-container'><p>Description : </p><p id='desc'>" + description + "</p></div><div id='price-container'><p>Price : </p><p id='price'>" + price + "</p></div><input type='submit' name='submit-add-cart' id='add-cart-submit' value='Add to cart'>";
+    var codeHtmlDetails = "" +
+        "<div id='desc-container'>" +
+            "<p>Description : </p>" +
+            "<p id='desc'>" + description + "</p>" +
+        "</div>" +
+        "<div id='price-container'>" +
+            "<p>Price : </p>" +
+            "<p id='price'>" + price + "</p>" +
+        "</div>" +
+        "<input type='submit' name='submit-add-cart' id='add-cart-submit' value='Add to cart'>";
     $(".modal #details-container").html(codeHtmlDetails);
 
     openModal();
 }
-
-/**
- *
- * @param responseString
- * @returns {Array}
- */
 function extractDetailsFromString(responseString)
 {
-    var responseArray = responseString.split("/");
-    return responseArray;
+    return responseString.split("/");
 }
 
 
@@ -151,7 +107,6 @@ function openModal()
 {
     $(".modal").show(150);
 }
-
 function hideModal()
 {
     $(".modal").hide(150);
