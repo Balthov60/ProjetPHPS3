7af984da37e3b9483956b22e7ad610a4e4ffab47
Balthov60 <Balthazarfrolin@gmail.com>
2017-11-06 22:38:15 +0100

    PHP Doc of Homepage

--- Project/classes/HomePage.php
+++ Project/classes/HomePage.php
@@ -10,18 +10,23 @@ class HomePage
 {
     private $sqlService;
 
-    const TABLE_JOIN = "image i JOIN image_keyword ik ON i.id_image = ik.id_image";
-
+    /**
+     * HomePage constructor.
+     *
+     * @param bool $isConnected
+     * @param bool $isAdmin
+     * @param SQLServices $sqlService
+     */
     function __construct($isConnected = false, $isAdmin = false, SQLServices $sqlService) {
-        $this->imageHandler = new ImageHandler($sqlService);
         $this->sqlService = $sqlService;
 
         new HeaderBar($isConnected, $isAdmin, 'HomePage', $sqlService);
         new ModalHandler();
 
-        echo "<div class=\"container bg-secondary\">";
+        echo "<div class='container bg-secondary main-page-content'>";
 
-        if (!isset($_GET["keywords"])) {
+        if (!isset($_GET["keywords"]))
+        {
             $this->displayAllImages();
         }
         else
@@ -35,7 +40,7 @@ class HomePage
     }
 
     /**
-     * Generic Method for Images Displaying (Handle Keywords in $_GET)
+     * Generic Method for Images Displaying. (Handle Keywords in $_GET)
      */
     private function displayAllImages() {
         $images = $this->sqlService->getData('image', 'distinct name_image');
@@ -44,6 +49,12 @@ class HomePage
             ImageHandler::displayImagesWithAutomaticResizing($images);
         }
     }
+
+    /**
+     * Display Images matching at least one of keywords.
+     *
+     * @param array $keywords list of keyword selected.
+     */
     private function displayImagesMatchingKeywords($keywords)
     {
         if(strpos($keywords, " ") !== false) // Check if there are multiple keywords
@@ -56,12 +67,17 @@ class HomePage
             $whereClause = "ik.keyword_name = '$keywords'";
         }
 
-        $images = $this->sqlService->getData(self::TABLE_JOIN, 'distinct name_image',
-            array("where" => $whereClause)
-        );
+        $images = $this->sqlService->getData("image i JOIN image_keyword ik ON i.id_image = ik.id_image",
+                                             'distinct name_image', array("where" => $whereClause));
         ImageHandler::displayImagesWithAutomaticResizing($images);
     }
 
+    /**
+     * Create SQL where clause with a list of keywords.
+     *
+     * @param $keywords
+     * @return string
+     */
     private function createWhereClauseForMultipleKeywords($keywords) {
         $whereClause = "";
         foreach ($keywords as $keyword) {
